50e03bf235b02799d4fabbcd727625f660950633
Akitsuyoshi <ka090824@gmail.com>
2022-11-21 00:29:53 +0900

    Add metris using prometheus

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const promMid = require('express-prometheus-middleware');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(promMid({
+    metricsPath: '/metrics',
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+}))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.get('/health', (req, res) => {
-    res.status(200).send()
-})
-
-app.get('/metrics', (req, res) => {
     res.status(200).send()
 })
 
