c48b30940b4a6245fd16022bac537fb89aaced81
Akitsuyoshi <ka090824@gmail.com>
2022-11-20 20:19:31 +0900

    Add interactable form

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const path = require('path')
+
+app.use(express.json())
+app.use(express.urlencoded())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.post('/echo_user_input', (req, res) => {
+    res.send(req.body.user_input)
 })
 
 app.listen(port, () => {
